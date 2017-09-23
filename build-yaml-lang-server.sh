b04c5a65666fc71e19d2d3e7fb17cbb2a9c8fd4e
liuderchi <liuderchi@gmail.com>
2017-09-24 01:06:26 +0800

    add yaml-lang-server to napa dependency
    - add shell script to build yaml lang server

---
+++ build-yaml-lang-server.sh
@@ -0,0 +1,15 @@
+cd node_modules/yaml-language-server/server &&
+
+npm install && npm run compile &&
+
+serverPath='../out/server/src/server.js'
+
+echo "Checking built yaml lang server path: $serverPath"
+
+if [ ! -f "$serverPath" ]
+then
+    echo "Yaml lang server $serverPath not found. Build failed."
+    exit -1;
+fi
+
+echo "Server build is success."
